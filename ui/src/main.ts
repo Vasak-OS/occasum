ed96d4cf9b3bc421bf17f7f08229b98fc7db585d
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-12-19 09:48:54 -0300

    clean UI

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,13 +1,9 @@
-import './assets/main.css';
-
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
 import App from './App.vue';
 
 const startApp = (channel: any) => {
   const app = createApp(App);
   app.config.globalProperties.$vsk = channel.objects.vsk;
-  app.use(createPinia());
 
   app.mount('#app');
 };

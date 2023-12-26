e79c198a704f5357189792582b5118190f35a7b2
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-12-26 14:31:16 -0300

    Add application metadata to Occasum.py

--- occasum.py
+++ occasum.py
@@ -2,11 +2,15 @@ import sys
 import signal
 from src.OccasumWindow import OccasumWindow
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtCore import Qt
 
 
 app = QApplication(sys.argv)
 
 if __name__ == "__main__":
+    app.setApplicationName("Occasum")
+    app.setApplicationVersion("0.0.1")
+    app.setOrganizationName("Vasak Group")
     window = OccasumWindow(app)
     window.show()
     signal.signal(signal.SIGINT, signal.SIG_DFL)  # Habilitar Ctrl+C

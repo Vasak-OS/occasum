c9036d954985d98d597423fd44d45a73d31ad70b
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-12-28 10:47:39 -0300

    Add StyleView component

---
+++ ui/src/views/StyleView.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'StyleView'
+});
+</script>
+
+<template>
+  <div>
+    <h1>Style</h1>
+    <p>Style page</p>
+  </div>
+</template>

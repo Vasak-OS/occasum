1097ed874b2a05ed44f0e71be5e2914525e2441f
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-02-19 14:36:31 -0300

    Añadida vista de red

--- ui/src/routers/index.ts
+++ ui/src/routers/index.ts
@@ -1,18 +1,25 @@
 import InfoView from '@/views/InfoView.vue';
+import NetworkViewVue from '@/views/NetworkView.vue';
 import StyleView from '@/views/StyleView.vue';
 
 const routers: Array<any> = [
   {
     title: 'Info',
-    image: 'Style',
+    image: 'help-info',
     tag: 'INFO',
     component: InfoView
   },
   {
     title: 'Style',
-    image: 'Style',
+    image: 'style',
     tag: 'STYLE',
     component: StyleView
+  },
+  {
+    title: 'Network',
+    image: 'networkmanager',
+    tag: 'NETWORK',
+    component: NetworkViewVue
   }
 ];
 
---
+++ ui/src/views/NetworkView.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div>
+    <h1>Network</h1>
+    <p>Style page</p>
+  </div>
+</template>

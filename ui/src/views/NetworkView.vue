87ea13ba0a39bb388db9ed537dbed6ec76fe8a76
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-02-20 13:59:42 -0300

    feat: Agregada información de red en la vista de red

--- ui/src/views/NetworkView.vue
+++ ui/src/views/NetworkView.vue
@@ -1,12 +1,24 @@
 <script lang="ts" setup>
-import { onMounted, ref, inject } from 'vue';
+import { onMounted, ref, inject, computed } from 'vue';
 
 const $vsk: any = inject('vsk');
-const networkInfo = ref({});
+const networkInfo = ref({
+  interface: 'enp4s0',
+  type: 'ethernet',
+  name: 'Conexi\u00f3n cableada 1',
+  connected: true,
+  icon: '/usr/share/icons/Fluent-teal-dark/symbolic/status/network-wired-symbolic.svg',
+  ip: '192.168.0.1'
+});
 
 async function setNetwork() {
-  networkInfo.value = await $vsk.getNetworkInfo();
+  networkInfo.value = JSON.parse(await $vsk.getNetworkInfo());
+  console.log('networkInfo', networkInfo.value);
 }
+
+const status = computed(() => {
+  return networkInfo.value.connected ? 'text-success' : 'text-danger';
+});
 
 onMounted(() => {
   setNetwork();
@@ -15,7 +27,29 @@ onMounted(() => {
 
 <template>
   <div>
-    <h1>Network</h1>
-    <p>{{ networkInfo }}</p>
+    <div class="card">
+      <div class="row g-0">
+        <div class="col-md-3">
+          <img :src="networkInfo.icon" class="img-fluid w-100" alt="Network icon" />
+        </div>
+        <div class="col-md-9">
+          <div class="card-body">
+            <h5 class="card-title">{{ networkInfo.interface }} <span class="network-status" :class="status">●</span></h5>
+              <p class="card-text">{{ networkInfo.name }} <br/> {{ networkInfo.ip }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+
+<style>
+.card {
+  background-color: var(--system-background);
+  border-radius: var(--system-border);
+  color: var(--system-text);
+  margin: 10px;
+  padding: 10px;
+}
+</style>

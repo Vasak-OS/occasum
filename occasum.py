4f438c48ce8375e74f457aa0151fb9b4c792ddb2
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-02-20 13:59:21 -0300

    fix: Añadir nombre de aplicación y icono de ventana en Occasum

--- occasum.py
+++ occasum.py
@@ -2,7 +2,7 @@ import sys
 import signal
 from src.OccasumWindow import OccasumWindow
 from PyQt6.QtWidgets import QApplication
-from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIcon
 
 
 app = QApplication(sys.argv)
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     app.setApplicationName("Occasum")
     app.setApplicationVersion("0.0.1")
     app.setOrganizationName("Vasak Group")
+    app.setApplicationDisplayName("Occasum")
+    app.setWindowIcon(QIcon.fromTheme("configurator"))
     window = OccasumWindow(app)
     window.show()
     signal.signal(signal.SIGINT, signal.SIG_DFL)  # Habilitar Ctrl+C
